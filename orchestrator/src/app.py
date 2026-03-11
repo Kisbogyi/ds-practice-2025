0be999f0f0c8f65dbbe8773a9b24aa8e45b4eca1
maare <maarekarmen@gmail.com>
2026-03-11 12:28:52 +0200

    add uuid to orders

--- orchestrator/src/app.py
+++ orchestrator/src/app.py
@@ -5,6 +5,7 @@ import grpc.aio
 from flask import Flask, request
 from flask_cors import CORS
 import json
+import uuid
 
 import suggestions.suggestions_pb2 as suggestions
 import suggestions.suggestions_pb2_grpc as suggestions_grpc
@@ -22,6 +23,7 @@ async def send_fraud_detection_grpc(
         username: str,
         order_amount: int,
         billing_address: str,
+        order_id: str,
 ):
     async with grpc.aio.insecure_channel('fraud_detection:50051') as channel:
         # Create a stub object.
@@ -34,15 +36,15 @@ async def send_fraud_detection_grpc(
         ))
     return response.is_fraud
 
-async def check_fraud(username: str, order_amount: int, billing_address: str):
+async def check_fraud(username: str, order_amount: int, billing_address: str, order_id: str):
     # Establish a connection with the fraud-detection gRPC service.
     logger.info(f"Calling FraudRequest endpoint with:  username: {username}, order amount: {order_amount}, billing_address: {billing_address}")
-    response = await send_fraud_detection_grpc(username, order_amount, billing_address)
+    response = await send_fraud_detection_grpc(username, order_amount, billing_address, order_id)
     logger.info(f"FraudRequest responded with: {response}")
     return response
 
 
-async def send_transaction_verification_grpc( card_number: str, order_amount: int):
+async def send_transaction_verification_grpc( card_number: str, order_amount: int, order_id: str):
     async with grpc.aio.insecure_channel('transaction_verification:50052') as channel:
         # Create a stub object.
         stub = transaction_verification_grpc.TransactionVerificationServiceStub(channel)
@@ -50,15 +52,15 @@ async def send_transaction_verification_grpc( card_number: str, order_amount: in
         response = await stub.VerifyTransaction(transaction_verification.VerificationRequest(card_number=card_number, order_amount=order_amount))
     return response.is_valid
 
-async def verify_transaction(card_number: str, order_amount: int):
+async def verify_transaction(card_number: str, order_amount: int, order_id: str):
     # Establish a connection with the fraud-detection gRPC service.
     logger.info(f"Calling TransactionVerification endpoint with:  card number: {card_number}, order amount: {order_amount}")
-    response = await send_transaction_verification_grpc(card_number, order_amount)
+    response = await send_transaction_verification_grpc(card_number, order_amount, order_id)
     logger.info(f"TransactionVerification responded with: {response}")
     return response
 
 
-async def get_suggestions_grpc(book_name: str) -> list[dict[str, str]]:
+async def get_suggestions_grpc(book_name: str, order_id: str) -> list[dict[str, str]]:
     async with grpc.aio.insecure_channel('suggestions:50053') as channel:
         # Create a stub object.
         stub = suggestions_grpc.SuggestionsServiceStub(channel)
@@ -68,10 +70,10 @@ async def get_suggestions_grpc(book_name: str) -> list[dict[str, str]]:
         recommended_books = [{"bookId": book[2], "title": book[0], "author": book[1]} for book in recommended_books]
     return recommended_books 
 
-async def suggest_books(book_name: str):
+async def suggest_books(book_name: str, order_id: str):
     # Establish a connection with the fraud-detection gRPC service.
     logger.info(f"Calling SuggestBook endpoint with:  book name: {book_name}")
-    response = await get_suggestions_grpc(book_name)
+    response = await get_suggestions_grpc(book_name, order_id)
     logger.info(f"SuggestBook responded with: {response}")
     return response
 
@@ -89,24 +91,28 @@ async def checkout():
     """
     # Get request object data to json
     request_data = json.loads(request.data)
+
+    # create uuid
+    order_id = str(uuid.uuid4())
+
     # Print request object data
-    logger.info(f"Checkout was called with Request Data: {request_data.get('items')}")
+    logger.info(f"Checkout was called for order {order_id} with Request Data: {request_data.get('items')}")
     credit_card_numer: str = request_data["creditCard"]["number"]
     order_amount: int = len(request_data["items"])
     billing_address: str = str(request_data["billingAddress"])
     logger.info(billing_address)
     username: str = request_data["user"]["name"]
     is_fraud, is_transaction_verified, book_suggestions = await asyncio.gather(
-        check_fraud(username, order_amount, billing_address),
-        verify_transaction(credit_card_numer, order_amount),
-        suggest_books(request_data.get('items')[0]["name"])
+        check_fraud(username, order_amount, billing_address, order_id),
+        verify_transaction(credit_card_numer, order_amount, order_id),
+        suggest_books(request_data.get('items')[0]["name"], order_id)
     )
 
     order_approve_text = "Order Rejected" if is_fraud or not is_transaction_verified else "Order Approved"
     #TODO: order approved depend on fraud-detection stuff
     logger.info(book_suggestions)
     order_status_response = json.dumps({
-        'orderId': '12345',
+        'orderId': order_id,
         'status': order_approve_text,
         'suggestedBooks': book_suggestions
     })
